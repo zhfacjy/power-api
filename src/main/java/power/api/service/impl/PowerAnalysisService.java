9444d29fd1cf2f785098fb9bebcc05c0953828d2
Asionf <841018243@qq.com>
2019-02-19 21:58:53 +0800

    完善用能报表接口

--- src/main/java/power/api/service/impl/PowerAnalysisService.java
+++ src/main/java/power/api/service/impl/PowerAnalysisService.java
@@ -12,6 +12,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
@@ -28,12 +29,48 @@ public class PowerAnalysisService implements IPowerAnalysisService {
     @Override
     public HashMap<String, Double> getPowerAnalysis(Integer dataType, String createAt) {
         HashMap<String, Double> result = new HashMap<>();
-        HashMap<String, Double> databases = new HashMap<>();
         Query query;
         SimpleDateFormat sdf;
         SimpleDateFormat sdf2;
+        // 日报
         if (dataType == 1) {
             sdf = new SimpleDateFormat("yyyyMMdd");
+            Integer today = Integer.valueOf(sdf.format(new Date()));
+            Integer paramData = Integer.valueOf(createAt);
+            // 如果查询日期比今天还要大，直接返回
+            if (today < paramData) {
+                result.put("00",0.00);
+                return result;
+            }
+            // 初始化
+            Integer times = 23;
+            // 如果查询日期等于今天，还要判断到达那个时间
+            if (today.equals(paramData)) {
+                sdf2 = new SimpleDateFormat("HH");
+                times = Integer.valueOf(sdf2.format(new Date()));
+            }
+            // 查询当前时间的所用电能
+            String findEnergy = "select sum(energy) as num,hours from (" +
+                    "select max(electric_energy) - min(electric_energy) as energy,DATE_FORMAT(create_at,'%H') as hours " +
+                    "from meter_record where DATE_FORMAT(create_at,'%Y%m%d') = ?1 GROUP BY meter,DATE_FORMAT(create_at,'%H')) as t " +
+                    "group by t.hours";
+            query = em.createNativeQuery(findEnergy);
+            query.setParameter(1,createAt);
+            query.unwrap(NativeQueryImpl.class).setResultTransformer(Transformers.ALIAS_TO_ENTITY_MAP);
+            JSONArray array = JSONArray.parseArray(JSON.toJSONString(query.getResultList()));
+            for (int i=0;i<=times;i++) {
+                String time = String.format("%02d",i);
+                result.put(time,0.0);
+                for (int j=0;j<array.size();j++) {
+                    if (array.getJSONObject(j).getString("hours").equals(time)) {
+                        result.put(time,array.getJSONObject(j).getDouble("num"));
+                        break;
+                    }
+                }
+            }
+        // 月报
+        } else if (dataType == 2) {
+            sdf = new SimpleDateFormat("yyyyMM");
             Integer today = Integer.valueOf(sdf.format(new Date()));
             Integer paramData = Integer.valueOf(createAt);
             if (today < paramData) {
@@ -41,55 +78,70 @@ public class PowerAnalysisService implements IPowerAnalysisService {
                 return result;
             }
             // 初始化
-            Integer times = 23;
+            Calendar calendar = Calendar.getInstance();
+            try{
+                calendar.setTime(sdf.parse(createAt));
+            } catch (Exception e) {
+                calendar.setTime(new Date());
+            }
+            Integer times = calendar.getActualMaximum(Calendar.DAY_OF_MONTH);
             if (today.equals(paramData)) {
-                sdf2 = new SimpleDateFormat("HH");
+                sdf2 = new SimpleDateFormat("dd");
                 times = Integer.valueOf(sdf2.format(new Date()));
             }
-            // 查询当天的各个时段的初始总电能
-            String sql = "select time as \"time\",sum(electric_energy) as \"energy\" from (" +
-                    "select min(electric_energy) as electric_energy,meter,time from (" +
-                    "select meter,truncate(electric_energy,2) as electric_energy,DATE_FORMAT(create_at,'%H') as time " +
-                    "from meter_record where DATE_FORMAT(create_at,'%Y%m%d') = '"+createAt.substring(0,8)+"') as t " +
-                    "group by meter,time) as b GROUP BY time";
-            query = em.createNativeQuery(sql);
-            // 返回dto写法 query.unwrap(NativeQueryImpl.class).setResultTransformer(Transformers.aliasToBean(clazz.class));
+            // 查询当前时间的所用电能
+            String findEnergy = "select sum(energy) as num,days from (" +
+                    "select max(electric_energy)-min(electric_energy) as energy,DATE_FORMAT(create_at,'%d') as days " +
+                    "from meter_record where DATE_FORMAT(create_at,'%Y%m') = ?1 " +
+                    "group by meter,DATE_FORMAT(create_at,'%d')) as t group by days";
+            query = em.createNativeQuery(findEnergy);
+            query.setParameter(1,createAt);
             query.unwrap(NativeQueryImpl.class).setResultTransformer(Transformers.ALIAS_TO_ENTITY_MAP);
-            List timeEnergy = query.getResultList();
-            if (timeEnergy.size()==0) {
-                return result;
-            }
-            JSONArray array = JSONArray.parseArray(JSON.toJSONString(timeEnergy));
-            // 查询最后一个时间段的最大电能值
-            String sql2 = "select sum(energy) as energy from (select meter,truncate(max(electric_energy),2) as energy from meter_record " +
-                    "where DATE_FORMAT(create_at,'%Y%m%d') = '"+createAt.substring(0,8)+"' GROUP BY meter) as t";
-            query = em.createNativeQuery(sql2);
-            Double max = (Double)query.getSingleResult();
-            for (int i=0;i<array.size();i++) {
-                JSONObject object = array.getJSONObject(i);
-                databases.put(object.getString("time"),object.getDouble("energy"));
-            }
-            for (int i=0;i<=times;i++) {
+            JSONArray array = JSONArray.parseArray(JSON.toJSONString(query.getResultList()));
+            for (int i=1;i<=times;i++){
                 String time = String.format("%02d",i);
-                int j = i + 1;
-                if (databases.containsKey(time)&&j<=times) {
-                    String time2 = String.format("%02d",j);
-                    if (databases.containsKey(time2)) {
-                        result.put(time,databases.get(time2) - databases.get(time));
-                    } else {
-                        result.put(time, 0.0);
+                result.put(time,0.0);
+                for (int j=0;j<array.size();j++) {
+                    if (array.getJSONObject(j).getString("days").equals(time)) {
+                        result.put(time,array.getJSONObject(j).getDouble("num"));
+                        break;
                     }
-                } else if (databases.containsKey(time)) {
-                    result.put(time,max - databases.get(time));
-                } else {
-                    result.put(time,0.0);
                 }
             }
-
-        } else if (dataType == 2) {
-
+        // 年报
         } else {
-
+            sdf = new SimpleDateFormat("yyyy");
+            Integer today = Integer.valueOf(sdf.format(new Date()));
+            Integer paramData = Integer.valueOf(createAt);
+            if (today < paramData) {
+                result.put("00",0.00);
+                return result;
+            }
+            // 初始化
+            Integer times = 12;
+            if (today.equals(paramData)) {
+                sdf2 = new SimpleDateFormat("MM");
+                times = Integer.valueOf(sdf2.format(new Date()));
+            }
+            // 查询当年的所用电能
+            String findEnergy = "select sum(energy) as num,months from (" +
+                    "select max(electric_energy)-min(electric_energy) as energy,meter," +
+                    "DATE_FORMAT(create_at,'%m') as months from meter_record " +
+                    "where DATE_FORMAT(create_at,'%Y') = ?1 GROUP BY meter,DATE_FORMAT(create_at,'%m')) as t group by t.months";
+            query = em.createNativeQuery(findEnergy);
+            query.setParameter(1,createAt);
+            query.unwrap(NativeQueryImpl.class).setResultTransformer(Transformers.ALIAS_TO_ENTITY_MAP);
+            JSONArray array = JSONArray.parseArray(JSON.toJSONString(query.getResultList()));
+            for (int i=1;i<=times;i++){
+                String time = String.format("%02d",i);
+                result.put(time,0.0);
+                for (int j=0;j<array.size();j++) {
+                    if (array.getJSONObject(j).getString("months").equals(time)) {
+                        result.put(time,array.getJSONObject(j).getDouble("num"));
+                        break;
+                    }
+                }
+            }
         }
         return result;
     }
