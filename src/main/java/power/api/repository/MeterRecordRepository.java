ca687e1c994db4b80f0cec9d218567442a3dc338
许百富 <1660866966@qq.com>
2019-02-20 17:25:03 +0800

    功能：电力极值报表-功率

--- src/main/java/power/api/repository/MeterRecordRepository.java
+++ src/main/java/power/api/repository/MeterRecordRepository.java
@@ -92,29 +92,146 @@ public interface MeterRecordRepository extends JpaRepository<MeterRecord, Intege
                                                      @Param("endAt") Timestamp endAt);
 
     @Query(value = "SELECT " +
-            "  m1.meter        AS                             meter, " +
-            "  m1.active_power AS                             limitValue, " +
-            "  DATE_FORMAT(m1.create_at, '%Y-%m-%d %H:%i:%S') createAt " +
-            "FROM meter_record AS m1 INNER JOIN ( " +
-            "                                     SELECT " +
-            "                                       meter, " +
-            "                                       MIN(active_power) AS max_active_power " +
-            "                                     FROM meter_record " +
-            "                                     WHERE create_at >= :startAt " +
-            "                                           AND create_at < :endAt " +
-            "                                     GROUP BY meter " +
-            "                                   ) AS m2 ON m1.meter = m2.meter AND m1.active_power = m2.max_active_power;", nativeQuery = true)
-    List<LimitReportDto> getMinActivePowerByCreateAt(@Param("startAt") Timestamp startAt,
-                                                     @Param("endAt") Timestamp endAt);
+            "  a.meter        AS                             meter, " +
+            "  a.active_power AS                             limitValue, " +
+            "  DATE_FORMAT(b.create_at, '%Y-%m-%d %H:%i:%S') createAt " +
+            "FROM ( " +
+            "       SELECT " +
+            "         MAX(active_power) AS active_power, " +
+            "         meter " +
+            "       FROM meter_record " +
+            "       WHERE DATE_FORMAT(create_at, :format) = :createAt " +
+            "       GROUP BY meter) AS a " +
+            "  LEFT JOIN meter_record b ON a.active_power = b.active_power " +
+            "                              AND DATE_FORMAT(b.create_at, :format) = :createAt AND b.meter = a.meter;", nativeQuery = true)
+    List<LimitReportDto> findMaxActivePowerByCreateAt(@Param("format") String format,
+                                                      @Param("createAt") String createAt);
+
+    @Query(value = "SELECT " +
+            "  a.meter        AS                             meter, " +
+            "  a.active_power AS                             limitValue, " +
+            "  DATE_FORMAT(b.create_at, '%Y-%m-%d %H:%i:%S') createAt " +
+            "FROM ( " +
+            "       SELECT " +
+            "         MIN(active_power) AS active_power, " +
+            "         meter " +
+            "       FROM meter_record " +
+            "       WHERE DATE_FORMAT(create_at, :format) = :createAt " +
+            "       GROUP BY meter) AS a " +
+            "  LEFT JOIN meter_record b ON a.active_power = b.active_power " +
+            "                              AND DATE_FORMAT(b.create_at, :format) = :createAt AND b.meter = a.meter;", nativeQuery = true)
+    List<LimitReportDto> findMinActivePowerByCreateAt(@Param("format") String format,
+                                                      @Param("createAt") String createAt);
 
     @Query(value = "SELECT " +
             "  meter, " +
             "  AVG(active_power)                  AS limitValue, " +
             "  DATE_FORMAT(create_at, '%Y-%m-%d') AS createAt " +
             "FROM meter_record " +
-            "WHERE create_at >= :startAt " +
-            "      AND create_at < :endAt " +
+            "WHERE DATE_FORMAT(create_at, :format) = :createAt " +
             "GROUP BY meter;", nativeQuery = true)
-    List<LimitReportDto> getAvgActivePowerByCreateAt(@Param("startAt") Timestamp startAt,
-                                                     @Param("endAt") Timestamp endAt);
+    List<LimitReportDto> findAvgActivePowerByCreateAt(@Param("format") String format,
+                                                      @Param("createAt") String createAt);
+
+
+    @Query(value = "SELECT " +
+            "  m1.meter              AS                       meter, " +
+            "  m1.max_reactive_power AS                       limitValue, " +
+            "  DATE_FORMAT(m2.create_at, '%Y-%m-%d %H:%i:%S') createAt " +
+            "FROM ( " +
+            "       SELECT " +
+            "         m2.meter, " +
+            "         MAX((m2.ia * m2.va + m2.ib * m2.vb + m2.ic * m2.vc) * (-cos((pi() / 2) + DEGREES( " +
+            "             ACOS(m2.active_power / ((m2.ia * m2.va + m2.ib * m2.vb + " +
+            "                                      m2.ic * m2.vc))))))) AS max_reactive_power " +
+            "       FROM meter_record AS m2 " +
+            "       WHERE DATE_FORMAT(create_at, :format) = :createAt " +
+            "       GROUP BY meter) AS m1 LEFT JOIN meter_record m2 ON m1.meter = m2.meter AND m1.max_reactive_power = " +
+            "                                                                                  (m2.ia * m2.va + m2.ib * m2.vb + " +
+            "                                                                                   m2.ic * m2.vc) * " +
+            "                                                                                  (-cos((pi() / 2) + DEGREES( " +
+            "                                                                                      ACOS(m2.active_power / (( " +
+            "                                                                                        m2.ia * m2.va + m2.ib * m2.vb + " +
+            "                                                                                        m2.ic * m2.vc))))));", nativeQuery = true)
+    List<LimitReportDto> findMaxReactivePowerByCreateAt(@Param("format") String format,
+                                                        @Param("createAt") String createAt);
+
+    @Query(value = "SELECT " +
+            "  m1.meter              AS                       meter, " +
+            "  m1.max_reactive_power AS                       limitValue, " +
+            "  DATE_FORMAT(m2.create_at, '%Y-%m-%d %H:%i:%S') createAt " +
+            "FROM ( " +
+            "       SELECT " +
+            "         m2.meter, " +
+            "         MIN((m2.ia * m2.va + m2.ib * m2.vb + m2.ic * m2.vc) * (-cos((pi() / 2) + DEGREES( " +
+            "             ACOS(m2.active_power / ((m2.ia * m2.va + m2.ib * m2.vb + " +
+            "                                      m2.ic * m2.vc))))))) AS max_reactive_power " +
+            "       FROM meter_record AS m2 " +
+            "       WHERE DATE_FORMAT(create_at, :format) = :createAt " +
+            "       GROUP BY meter) AS m1 LEFT JOIN meter_record m2 ON m1.meter = m2.meter AND m1.max_reactive_power = " +
+            "                                                                                  (m2.ia * m2.va + m2.ib * m2.vb + " +
+            "                                                                                   m2.ic * m2.vc) * " +
+            "                                                                                  (-cos((pi() / 2) + DEGREES( " +
+            "                                                                                      ACOS(m2.active_power / (( " +
+            "                                                                                        m2.ia * m2.va + m2.ib * m2.vb + " +
+            "                                                                                        m2.ic * m2.vc))))));", nativeQuery = true)
+    List<LimitReportDto> findMinReactivePowerByCreateAt(@Param("format") String format,
+                                                        @Param("createAt") String createAt);
+
+    @Query(value = "SELECT " +
+            "  m2.meter                                          AS meter, " +
+            "  AVG((m2.ia * m2.va + m2.ib * m2.vb + m2.ic * m2.vc) * (-cos((pi() / 2) + DEGREES( " +
+            "      ACOS(m2.active_power / ((m2.ia * m2.va + m2.ib * m2.vb + " +
+            "                               m2.ic * m2.vc))))))) AS limitValue, " +
+            "  DATE_FORMAT(m2.create_at, '%Y-%m-%d')             AS createAt " +
+            "FROM meter_record AS m2 " +
+            "WHERE DATE_FORMAT(create_at, :format) = :createAt " +
+            "GROUP BY meter;", nativeQuery = true)
+    List<LimitReportDto> findAvgReactivePowerByCreateAt(@Param("format") String format,
+                                                        @Param("createAt") String createAt);
+
+    @Query(value = "SELECT " +
+            "  m1.meter AS                                    meter, " +
+            "  m1.power AS                                    limitValue, " +
+            "  DATE_FORMAT(m2.create_at, '%Y-%m-%d %H:%i:%S') createAt " +
+            "FROM ( " +
+            "       SELECT " +
+            "         m2.meter, " +
+            "         MAX(m2.ia * m2.va + m2.ib * m2.vb + m2.ic * m2.vc) AS power " +
+            "       FROM meter_record AS m2 " +
+            "       WHERE DATE_FORMAT(create_at, :format) = :createAt " +
+            "       GROUP BY meter) AS m1 LEFT JOIN meter_record m2 ON m1.meter = m2.meter " +
+            "                                                          AND m1.power = (m2.ia * m2.va " +
+            "                                                                          + m2.ib * m2.vb " +
+            "                                                                          + m2.ic * m2.vc);", nativeQuery = true)
+    List<LimitReportDto> findMaxApparentPowerByCreateAt(@Param("format") String format,
+                                                        @Param("createAt") String createAt);
+
+    @Query(value = "SELECT " +
+            "  m1.meter AS                                    meter, " +
+            "  m1.power AS                                    limitValue, " +
+            "  DATE_FORMAT(m2.create_at, '%Y-%m-%d %H:%i:%S') createAt " +
+            "FROM ( " +
+            "       SELECT " +
+            "         m2.meter, " +
+            "         MIN(m2.ia * m2.va + m2.ib * m2.vb + m2.ic * m2.vc) AS power " +
+            "       FROM meter_record AS m2 " +
+            "       WHERE DATE_FORMAT(create_at, :format) = :createAt " +
+            "       GROUP BY meter) AS m1 LEFT JOIN meter_record m2 ON m1.meter = m2.meter " +
+            "                                                          AND m1.power = (m2.ia * m2.va " +
+            "                                                                          + m2.ib * m2.vb " +
+            "                                                                          + m2.ic * m2.vc);", nativeQuery = true)
+    List<LimitReportDto> findMinApparentPowerByCreateAt(@Param("format") String format,
+                                                        @Param("createAt") String createAt);
+
+
+    @Query(value = "SELECT " +
+            "  m2.meter                                           AS meter, " +
+            "  AVG(m2.ia * m2.va + m2.ib * m2.vb + m2.ic * m2.vc) AS limitValue, " +
+            "  DATE_FORMAT(m2.create_at, '%Y-%m-%d')              AS createAt " +
+            "FROM meter_record AS m2 " +
+            "WHERE DATE_FORMAT(create_at, :format) = :createAt " +
+            "GROUP BY meter;", nativeQuery = true)
+    List<LimitReportDto> findAvgApparentPowerByCreateAt(@Param("format") String format,
+                                                        @Param("createAt") String createAt);
 }
