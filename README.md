a08bb3c8f3bc9a8c66f52c6e9ed9fc2eaa4e1c4b
许百富 <1660866966@qq.com>
2019-02-23 16:00:38 +0800

    增加文档描述

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# 运行
+# 开发运行
 
 ## 前置条件
 1. 在IDEA中安装并配置`Lombok`插件
@@ -6,3 +6,27 @@
     2. 在IDEA中左上角点击 `file` -> `setting` -> `Build, Execution, Deployment` -> `Compiler, Annotation Processors`。勾选`Enable Annotation Processing`
 2. 系统中已安装和配置`Gradle`
     1. (官网)[https://gradle.org/]
+    
+# 服务器部署
+
+## 服务器环境
+```
+操作系统：Ubuntu 16.04
+JDK版本：1.8
+Mysql版本：5.7
+```
+
+## 数据库配置
+- 初始化数据库：执行`resources`文件夹下的`sql.sql`文件
+- 修改数据库配置
+  - 开发环境配置修改：`resources`文件夹下的`application-dev.yml`文件
+  - 本地环境配置修改：`resources`文件夹下的`application-pord.yml`文件
+  - 修改字段为`spring.datasource.druid.url`
+- 注意同时修改power-mqtt项目中的数据库配置，相同位置
+- 注意同时修改power-mqtt项目中的数据库配置，相同位置
+- 注意同时修改power-mqtt项目中的数据库配置，相同位置
+
+## 步骤
+1. 将项目打包成Jar包
+2. 上传到服务器
+3. 运行`nohup java -jar [文件名].jar &`命令启动应用
--- src/main/resources/sql.sql
+++ src/main/resources/sql.sql
@@ -1,206 +1,144 @@
-SELECT
-  DATE_FORMAT(mr.create_at, '%Y-%m-%d') days,
-  MAX(mr.active_power)                  max_active_power,
-  AVG(mr.active_power)                  avg_active_power,
-  MIN(mr.active_power)                  min_active_power,
-  count(mr.id)                          count
-FROM meter_record mr
-WHERE UNIX_TIMESTAMP(mr.create_at) >= UNIX_TIMESTAMP('2019-02-16 00:00:00')
-      AND UNIX_TIMESTAMP(mr.create_at) <= UNIX_TIMESTAMP('2019-02-17 23:59:59')
-GROUP BY days;
+CREATE TABLE dict
+(
+  id        INT auto_increment
+  PRIMARY KEY,
+  label     VARCHAR(50) NULL comment '字典表的名',
+  value     VARCHAR(50) NULL comment '字典表的值',
+  parent_id INT         NOT NULL comment '父id',
+  type      VARCHAR(30) NULL comment '类型',
+  create_at TIMESTAMP   NULL comment '创建时间'
+)
+  comment '字典表';
 
+INSERT INTO dict (label, value, parent_id, type, create_at) VALUES ('有功功率', 'active_power', 0, 'electric_category_single_day', '2019-02-15 03:21:57');
+INSERT INTO dict (label, value, parent_id, type, create_at) VALUES ('A相', 'phase_a', 3, 'electric_category_single_day', '2019-02-15 03:26:21');
+INSERT INTO dict (label, value, parent_id, type, create_at) VALUES ('B相', 'phase_b', 3, 'electric_category_single_day', '2019-02-15 03:26:45');
+INSERT INTO dict (label, value, parent_id, type, create_at) VALUES ('C相', 'phase_c', 3, 'electric_category_single_day', '2019-02-15 03:27:13');
+INSERT INTO dict (label, value, parent_id, type, create_at) VALUES ('总有功功率', 'total_active_power', 3, 'electric_category_single_day', '2019-02-15 03:29:22');
+INSERT INTO dict (label, value, parent_id, type, create_at) VALUES ('电流', 'electric_current', 0, 'electric_category_single_day', '2019-02-15 03:38:30');
+INSERT INTO dict (label, value, parent_id, type, create_at) VALUES ('相电压', 'phase_voltage', 0, 'electric_category_single_day', '2019-02-15 07:39:34');
+INSERT INTO dict (label, value, parent_id, type, create_at) VALUES ('线电压', 'line_voltage', 0, 'electric_category_single_day', '2019-02-15 07:40:07');
+INSERT INTO dict (label, value, parent_id, type, create_at) VALUES ('无功率功率', 'reactive_power', 0, 'electric_category_single_day', '2019-02-15 07:41:22');
+INSERT INTO dict (label, value, parent_id, type, create_at) VALUES ('视在功率', 'apparent_power', 0, 'electric_category_single_day', '2019-02-15 07:41:56');
+INSERT INTO dict (label, value, parent_id, type, create_at) VALUES ('功率因数', 'power_factor', 0, 'electric_category_single_day', '2019-02-16 16:00:16');
 
-SELECT
-  *,
-  max(active_power)
-FROM (SELECT *
-      FROM meter_record mr1
-      WHERE UNIX_TIMESTAMP(mr1.create_at) >= UNIX_TIMESTAMP('2019-02-17 00:00:00')
-            AND UNIX_TIMESTAMP(mr1.create_at) <= UNIX_TIMESTAMP('2019-02-17 23:59:59')
-      ORDER BY mr1.active_power DESC) tmr
-GROUP BY meter;
+CREATE TABLE dict_line_counters_id__transformer_substation_id
+(
+  id                        INT auto_increment
+  PRIMARY KEY,
+  transformer_substation_id INT NULL comment '变电站id',
+  dict_line_counters_id     INT NULL comment '进线柜id'
+)
+  comment '进线柜和变电站的关联';
 
-SELECT
-  g1.name,
-  g1.publisher,
-  g1.price,
-  g2.price,
-  g1.released_at
-FROM games AS g1
-  LEFT OUTER JOIN games AS g2
-    ON g1.released_at = g2.released_at AND g1.price < g2.price
-WHERE g2.price IS NULL;
+CREATE TABLE dict_meter_value__dict_line_counters_id
+(
+  id                    INT auto_increment
+  PRIMARY KEY,
+  dict_meter_value      VARCHAR(2) NULL comment '电表号',
+  dict_line_counters_id INT        NULL comment '进线柜id'
+)
+  comment '电表和进线柜的关联';
 
-SELECT
-  m1.meter        AS                             meter,
-  m1.active_power AS                             limitValue,
-  DATE_FORMAT(m1.create_at, '%Y-%m-%d %H:%i:%S') createAt
-FROM meter_record AS m1 INNER JOIN (
-                                     SELECT
-                                       meter,
-                                       MAX(active_power) AS max_active_power
-                                     FROM meter_record
-                                     WHERE UNIX_TIMESTAMP(create_at) >= UNIX_TIMESTAMP('2019-02-16 00:00:00')
-                                           AND UNIX_TIMESTAMP(create_at) < UNIX_TIMESTAMP('2019-02-17 00:00:00')
-                                     GROUP BY meter
-                                   ) AS m2 ON m1.meter = m2.meter AND m1.active_power = m2.max_active_power
+CREATE TABLE games
+(
+  game_id     BIGINT auto_increment
+  PRIMARY KEY,
+  name        VARCHAR(30)                         NULL,
+  price       BIGINT                              NULL,
+  released_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+  publisher   VARCHAR(100)                        NULL,
+  CONSTRAINT game_id
+  UNIQUE (game_id)
+);
 
+CREATE TABLE meter_record
+(
+  id              INT auto_increment
+  PRIMARY KEY,
+  central_node    VARCHAR(2) NULL comment '中心节点',
+  meter           VARCHAR(2) NULL comment '电表',
+  command         VARCHAR(2) NULL comment '命令',
+  early_warning   VARCHAR(2) NULL comment '预警',
+  ua              FLOAT      NULL comment 'A相电压',
+  ub              FLOAT      NULL comment 'B相电压',
+  uc              FLOAT      NULL comment 'C相电压',
+  ia              FLOAT      NULL comment 'A相电流',
+  ib              FLOAT      NULL comment 'B相电流',
+  ic              FLOAT      NULL comment 'C相电流',
+  active_power    DOUBLE     NULL comment '功率',
+  electric_energy DOUBLE     NULL comment '电能',
+  temperature     INT        NULL comment '温度',
+  current_limit   INT        NULL comment '电流上限',
+  crc             VARCHAR(4) NULL comment 'CRC校验码',
+  create_at       TIMESTAMP  NULL comment '记录时间',
+  pfa             FLOAT      NULL comment 'A相功率因数',
+  pfb             FLOAT      NULL comment 'B相功率因数',
+  pfc             FLOAT      NULL comment 'C相功率因数',
+  frequency       FLOAT      NULL comment '频率'
+)
+  comment '电表数据记录';
 
-SELECT
-  m2.meter        AS                             meter,
-  m2.active_power AS                             limitValue,
-  DATE_FORMAT(m2.create_at, '%Y-%m-%d %H:%i:%S') createAt
-FROM
-  (SELECT
-     meter,
-     MAX(active_power) AS max_active_power
-   FROM meter_record
-   WHERE UNIX_TIMESTAMP(create_at) >= UNIX_TIMESTAMP('2019-02-16 00:00:00')
-         AND UNIX_TIMESTAMP(create_at) < UNIX_TIMESTAMP('2019-02-17 00:00:00')
-   GROUP BY meter) AS m1 LEFT JOIN meter_record m2 ON m2.active_power = m1.max_active_power AND m1.meter = m2.meter;
+CREATE TABLE patrol_plan
+(
+  id                        INT auto_increment
+  PRIMARY KEY,
+  transformer_substation_id INT                                 NULL comment '变电站id',
+  dict_patrol_group_id      INT                                 NULL comment '巡检组别id',
+  dict_patrol_category_id   INT                                 NULL comment '巡检类别id',
+  dict_patrol_nature_id     INT                                 NULL comment '巡检性质id',
+  patrol_content            VARCHAR(100)                        NULL comment '巡检内容',
+  dict_plan_status_id       INT                                 NULL comment '执行状态',
+  review_at                 TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL comment '审核日期',
+  review_by                 INT                                 NULL comment '审核人',
+  finish_at                 TIMESTAMP                           NULL comment '完成日期',
+  create_by                 INT                                 NULL comment '创建人',
+  create_at                 TIMESTAMP                           NULL comment '起始日期'
+)
+  comment '巡检计划表';
 
-SELECT
-  a.meter        AS                             meter,
-  a.active_power AS                             limitValue,
-  DATE_FORMAT(b.create_at, '%Y-%m-%d %H:%i:%S') createAt
-FROM (
-       SELECT
-         max(active_power) AS active_power,
-         meter
-       FROM meter_record
-       WHERE DATE_FORMAT(create_at, '%Y%m%d') = '20190216'
-       GROUP BY meter) AS a
-  LEFT JOIN meter_record b ON a.active_power = b.active_power
-                              AND DATE_FORMAT(b.create_at, '%Y%m%d') = '20190216' AND b.meter = a.meter;
+CREATE TABLE transformer_substation
+(
+  id                         INT auto_increment
+  PRIMARY KEY,
+  label                      VARCHAR(2)   NULL comment '变电站名称',
+  voltage_level              VARCHAR(10)  NULL comment '电压等级',
+  installed_capacity         VARCHAR(10)  NULL comment '装机容量',
+  declared_demand            VARCHAR(10)  NULL comment '申报需量',
+  measurement_control_number VARCHAR(10)  NULL comment '测控装置',
+  address                    VARCHAR(100) NULL comment '所在地址',
+  create_at                  TIMESTAMP    NULL comment '创建时间'
+)
+  comment '变电站表';
 
-SELECT
-  meter,
-  AVG(active_power)                  AS limitValue,
-  DATE_FORMAT(create_at, '%Y-%m-%d') AS createAt
-FROM meter_record
-WHERE DATE_FORMAT(create_at, '%Y-%m-%d') = '2019-02-16'
-GROUP BY meter;
+CREATE TABLE user
+(
+  id        INT auto_increment
+  PRIMARY KEY,
+  username  VARCHAR(20)  NULL comment '用户名',
+  password  VARCHAR(256) NULL comment '密码',
+  mobile    VARCHAR(11)  NULL comment '手机号',
+  create_at TIMESTAMP    NULL comment '创建时间'
+)
+  comment '用户表';
 
-SELECT
-  m1.meter,
-  ((m1.ia * m1.ua + m1.ib * m1.ub + m1.ic * m1.uc) * (-cos((pi() / 2) + DEGREES(
-      acos(m1.active_power / (m1.ia * m1.ua + m1.ib * m1.ub + m1.ic * m1.uc)))))) AS max_reactive_power
-FROM meter_record AS m1 INNER JOIN (
-                                     SELECT
-                                       m2.meter,
-                                       max((m2.ia * m2.ua + m2.ib * m2.ub + m2.ic * m2.uc) * (-cos((pi() / 2) + DEGREES(
-                                           acos(m2.active_power / ((m2.ia * m2.ua + m2.ib * m2.ub +
-                                                                    m2.ic * m2.uc))))))) AS max_reactive_power
-                                     FROM meter_record AS m2
-                                     WHERE UNIX_TIMESTAMP(create_at) >= UNIX_TIMESTAMP('2019-02-16 00:00:00')
-                                           AND UNIX_TIMESTAMP(create_at) < UNIX_TIMESTAMP('2019-02-17 00:00:00')
-                                     GROUP BY meter) AS m2 ON m1.meter = m2.meter AND m2.max_reactive_power = (
-  (m1.ia * m1.ua + m1.ib * m1.ub + m1.ic * m1.uc) *
-  (-cos((pi() / 2) + DEGREES(acos(m1.active_power / ((m1.ia * m1.ua + m1.ib * m1.ub + m1.ic * m1.uc)))))));
+CREATE TABLE user_id__dict_patrol_group_id
+(
+  id                   INT auto_increment
+  PRIMARY KEY,
+  user_id              INT       NULL comment '用户id',
+  dict_patrol_group_id INT       NULL comment '巡检组别id',
+  create_at            TIMESTAMP NULL comment '创建时间'
+)
+  comment '用户和巡检组的关系';
 
-SELECT
-  m1.meter              AS                       meter,
-  m1.max_reactive_power AS                       limitValue,
-  DATE_FORMAT(m2.create_at, '%Y-%m-%d %H:%i:%S') createAt
-FROM (
-       SELECT
-         m2.meter,
-         MAX((m2.ia * m2.ua + m2.ib * m2.ub + m2.ic * m2.uc) * (-cos((pi() / 2) + DEGREES(
-             ACOS(m2.active_power / ((m2.ia * m2.ua + m2.ib * m2.ub +
-                                      m2.ic * m2.uc))))))) AS max_reactive_power
-       FROM meter_record AS m2
-       WHERE DATE_FORMAT(create_at, '%Y-%m') = '2019-02'
-       GROUP BY meter) AS m1 LEFT JOIN meter_record m2 ON m1.meter = m2.meter AND m1.max_reactive_power =
-                                                                                  (m2.ia * m2.ua + m2.ib * m2.ub +
-                                                                                   m2.ic * m2.uc) *
-                                                                                  (-cos((pi() / 2) + DEGREES(
-                                                                                      ACOS(m2.active_power / ((
-                                                                                        m2.ia * m2.ua + m2.ib * m2.ub +
-                                                                                        m2.ic * m2.uc))))));
+CREATE TABLE user_id__patrol_plan_id
+(
+  id             INT auto_increment
+  PRIMARY KEY,
+  user_id        INT       NULL comment '用户id',
+  patrol_plan_id INT       NULL comment '巡检计划id',
+  create_at      TIMESTAMP NULL comment '创建时间'
+)
+  comment '计划的执行成员';
 
-SELECT
-  m2.meter                                          AS meter,
-  AVG((m2.ia * m2.ua + m2.ib * m2.ub + m2.ic * m2.uc) * (-cos((pi() / 2) + DEGREES(
-      ACOS(m2.active_power / ((m2.ia * m2.ua + m2.ib * m2.ub +
-                               m2.ic * m2.uc))))))) AS limitValue,
-  DATE_FORMAT(m2.create_at, '%Y-%m-%d')             AS createAt
-FROM meter_record AS m2
-WHERE DATE_FORMAT(create_at, '%Y-%m-%d') = '2019-02-18'
-GROUP BY meter;
-
-SELECT
-  m1.meter AS                                    meter,
-  m1.power AS                                    limitValue,
-  DATE_FORMAT(m2.create_at, '%Y-%m-%d %H:%i:%S') createAt
-FROM (
-       SELECT
-         m2.meter,
-         MAX(m2.ia * m2.ua + m2.ib * m2.ub + m2.ic * m2.uc) AS power
-       FROM meter_record AS m2
-       WHERE DATE_FORMAT(create_at, '%Y-%m-18') = '2019-02-18'
-       GROUP BY meter) AS m1 LEFT JOIN meter_record m2 ON m1.meter = m2.meter
-                                                          AND m1.power = (m2.ia * m2.ua
-                                                                          + m2.ib * m2.ub
-                                                                          + m2.ic * m2.uc);
-
-SELECT
-  m2.meter                                           AS meter,
-  AVG(m2.ia * m2.ua + m2.ib * m2.ub + m2.ic * m2.uc) AS limitValue,
-  DATE_FORMAT(m2.create_at, '%Y-%m-%d')              AS createAt
-FROM meter_record AS m2
-WHERE DATE_FORMAT(create_at, '%Y-%m-%d') = '2019-02-18'
-GROUP BY meter;
-
-SELECT
-  mr1.active_power,
-  mr1.create_at
-FROM meter_record mr1
-WHERE UNIX_TIMESTAMP(mr1.create_at) >= UNIX_TIMESTAMP('2019-02-17 00:00:00')
-      AND UNIX_TIMESTAMP(mr1.create_at) < UNIX_TIMESTAMP('2019-02-18 00:00:00')
-ORDER BY mr1.active_power DESC;
-
-SELECT
-  DATE_FORMAT(mr.create_at, '%Y-%m-%d') AS createAt,
-  MAX(mr.active_power)                  AS maxHolder,
-  AVG(mr.active_power)                  AS avgHolder,
-  MIN(mr.active_power)                  AS minHolder
-FROM meter_record mr
-WHERE UNIX_TIMESTAMP(mr.create_at) >= UNIX_TIMESTAMP('2019-02-16 00:00:00')
-      AND UNIX_TIMESTAMP(mr.create_at) <= UNIX_TIMESTAMP('2019-02-17 23:59:59')
-GROUP BY DATE_FORMAT(mr.create_at, '%Y-%m-%d');
-
-
-SELECT (4 * HOUR(mr.create_at) + FLOOR(MINUTE(mr.create_at) / 1)) minutes
-FROM meter_record mr
-WHERE UNIX_TIMESTAMP(mr.create_at) >= UNIX_TIMESTAMP('2019-02-15 02:35:00')
-      AND UNIX_TIMESTAMP(mr.create_at) <= UNIX_TIMESTAMP('2019-02-15 02:36:00')
-GROUP BY minutes;
-
-SELECT mr.create_at
-FROM meter_record mr
-WHERE UNIX_TIMESTAMP(mr.create_at) >= UNIX_TIMESTAMP('2019-02-17 20:00:00')
-      AND UNIX_TIMESTAMP(mr.create_at) <= UNIX_TIMESTAMP('2019-02-17 20:36:00');
-
-SELECT
-  AVG(mr.ua)           AS ua,
-  AVG(mr.ub)           AS ub,
-  AVG(mr.uc)           AS uc,
-  AVG(mr.ia)           AS ia,
-  AVG(mr.ib)           AS ib,
-  AVG(mr.ic)           AS ic,
-  AVG(mr.active_power) AS active_power,
-  date_add('2019-02-17 00:00:00', INTERVAL 15 * floor(timestampdiff(MINUTE, '2019-02-17 00:00:00', mr.create_at) / 15)
-  MINUTE)              AS create_at
-FROM meter_record mr
-WHERE UNIX_TIMESTAMP(mr.create_at) >= UNIX_TIMESTAMP('2019-02-17 00:00:00')
-      AND UNIX_TIMESTAMP(mr.create_at) < UNIX_TIMESTAMP('2019-02-18 00:00:00')
-GROUP BY floor(timestampdiff(MINUTE, '2019-02-17 00:00:00', mr.create_at) / 15);
-
-SELECT floor(TIMESTAMPDIFF(MINUTE, '2019-02-17 00:00:00', '2019-02-17 00:01:01'));
-SELECT TIMESTAMPDIFF(MINUTE, '2019-02-17 00:00:00', '2019-02-17 00:01:30');
-
-SELECT @@sql_mode;
-
-SET SQL_MODE = 'STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION';
-
-SET GLOBAL sql_mode = (SELECT REPLACE(@@sql_mode, 'ONLY_FULL_GROUP_BY', ''));
